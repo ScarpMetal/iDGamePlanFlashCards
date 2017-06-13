fb015475f2c483b40987326d798d671e78f11ff5
matthewgraham <mpgraham917@gmail.com>
2017-06-13 02:23:58 -0400

    connected flashcard data to text and picker views

---
+++ FlashCard App/Flashcards.swift
@@ -0,0 +1,60 @@
+//
+//  Flashcards.swift
+//  FlashCard App
+//
+//  Created by Matthew Graham on 6/13/17.
+//  Copyright © 2017 iD Tech. All rights reserved.
+//
+
+import Foundation
+
+class Flashcard {
+    var question : String
+    
+    var options : [String]
+    var correctAnswer : String
+    
+    init(question : String, options : [String]){
+        self.question = question
+        self.options = options
+        
+        self.correctAnswer = options[0]
+    }
+}
+
+class CardCollection {
+    
+    // Single instance of the card collection
+    public static var instance : CardCollection = CardCollection()
+    
+    public private(set) var cards : [Flashcard]
+    
+    public private(set) var currentIndex : Int
+    
+    public var currentCard : Flashcard {
+        get { return cards[currentIndex] }
+    }
+    
+    private init(){
+        cards = [
+            Flashcard(question: "Test Question", options: ["True","False"]),
+            Flashcard(question: "What's the capitol of Washington state?", options:["Olympia", "Tacoma", "Seattle", "Spokane"]),
+            Flashcard(question: "Where is iD Tech's headquarters?", options:["Campbell, CA", "Albequerque, NM", "Beijing, CN"]),
+            Flashcard(question: "What is Matt's favorite food?", options:["Dumplings", "Mac and Cheese", "Pizza"])
+        ]
+        
+        currentIndex = 0;
+    }
+    
+    public func nextQuestion() {
+        currentIndex += 1
+        if(currentIndex >= cards.count){
+            currentIndex = 0
+        }
+    }
+    
+    public func checkAnswer(_ selectedAnswer : Int) -> Bool {
+        return currentCard.options[selectedAnswer] == currentCard.correctAnswer
+    }
+    
+}
